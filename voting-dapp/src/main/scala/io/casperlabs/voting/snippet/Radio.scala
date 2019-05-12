fd376abbfc10d27b66c71f9b70673efdb6b09e9a
birchmd <birchmd@casperlabs.io>
2019-05-12 13:08:59 -0400

    DEMO: State query and displaying of results

--- voting-dapp/src/main/scala/io/casperlabs/voting/snippet/Radio.scala
+++ voting-dapp/src/main/scala/io/casperlabs/voting/snippet/Radio.scala
@@ -2,6 +2,7 @@ package io.casperlabs.voting.snippet
 
 import cats.implicits._
 import cats.effect.Sync
+import cats.effect.concurrent.Ref
 
 import com.google.protobuf.ByteString
 
@@ -18,8 +19,10 @@ import monix.execution.Scheduler
 
 import net.liftweb.common._
 import net.liftweb.util.Helpers._
-import net.liftweb.http.SHtml
+import net.liftweb.http.{S, SHtml}
 import net.liftweb.http.SHtml.ChoiceHolder
+
+import scala.xml.{Elem, XML}
 
 object SatoshiIdentity extends Enumeration {
   type SatoshiIdentity = Value
@@ -33,8 +36,70 @@ object SatoshiIdentity extends Enumeration {
   val neverKnow = Value("The world will never know")
 }
 
+object Querying {
+
+  private def message(block: String, id: Int): QueryStateRequest =
+    QueryStateRequest(block, "address", Voting.accountAddressBase16, s"vote/$id")
+
+  val latestBlock: Ref[Task, Option[Voting.BlockHash]] =
+    Ref.unsafe[Task, Option[Voting.BlockHash]](None)
+
+  def query[F[_]](block: String)(implicit s: Sync[F], d: DeployService[F]): F[Vector[Int]] =
+    (0 until SatoshiIdentity.maxId).toVector.traverse { id =>
+      d.queryState(message(block, id)).rethrow.flatMap { result =>
+        if (result.startsWith("integer"))
+          result.split(':')(1).trim.toInt.pure[F]
+        else
+          Sync[F].raiseError(new RuntimeException(s"Unexpected result from state query: $result"))
+      }
+    }
+
+  def queryLatest(implicit d: DeployService[Task]): Task[(Voting.BlockHash, Vector[Int])] =
+    latestBlock.get.flatMap {
+      case Some(block) => query[Task](block).map(block -> _)
+      case None        => Task.raiseError(new RuntimeException("No latest block to query!"))
+    }
+
+  def parsedResults(results: Vector[Int]): Vector[(Int, SatoshiIdentity.SatoshiIdentity, Int)] =
+    results.zipWithIndex
+      .map {
+        case (votes, id) => (votes, SatoshiIdentity(id))
+      }
+      .sortBy(-_._1) // sort in decending order
+      .zipWithIndex
+      .map {
+        case ((votes, id), rank) =>
+          (rank + 1, id, votes)
+      }
+
+  def htmlResults(block: Voting.BlockHash, results: Vector[Int]): Elem = XML.loadString(
+    s"""|<span>
+        |  <h1>Results as of block: $block</h1>
+        |  <table border="1">
+        |    <tr>
+        |      <th>Rank</th>
+        |      <th>Candidate</th>
+        |      <th>Votes</th>
+        |    </tr>
+        |    ${parsedResults(results)
+         .map { case (rank, id, votes) => s"<tr><td>$rank</td><td>$id</td><td>$votes</td></tr>" }
+         .mkString("\n")}
+        |  </table>
+        |</span> 
+        |""".stripMargin
+  )
+
+  def render = {
+    implicit val scheduler     = Radio.scheduler
+    val (latestBlock, results) = queryLatest(Voting.deployService).runSyncUnsafe()
+
+    "#theResults" #> htmlResults(latestBlock, results)
+  }
+}
+
 object Voting {
   import SatoshiIdentity._
+  type BlockHash = String
 
   val deployService = new GrpcDeployService("3.16.244.129", 40401)
   val voteContract: File =
@@ -42,11 +107,14 @@ object Voting {
       "/mnt/c/Users/mbirc/Documents/CasperLabs/CasperLabs/voting-dapp/rust/call/target/wasm32-unknown-unknown/release/vote.wasm"
     )
 
-  val voteWasm: ByteString = ByteString.copyFrom(Files.readAllBytes(voteContract.toPath))
+  val voteWasm: ByteString         = ByteString.copyFrom(Files.readAllBytes(voteContract.toPath))
+  val accountAddressBase16: String = "3030303030303030303030303030303030303030"
   val accountAddress: ByteString =
-    ByteString.copyFrom(Base16.decode("3030303030303030303030303030303030303030"))
+    ByteString.copyFrom(Base16.decode(accountAddressBase16))
 
-  def vote[F[_]](option: SatoshiIdentity)(implicit s: Sync[F], d: DeployService[F]): F[Unit] = {
+  def vote[F[_]](
+      option: SatoshiIdentity
+  )(implicit s: Sync[F], d: DeployService[F]): F[BlockHash] = {
     val i           = option.id
     val iBytes      = ByteString.copyFrom(Array(1, 0, 0, 0, 4, 0, 0, 0, i, 0, 0, 0).map(_.toByte))
     val sessionCode = DeployCode().withCode(voteWasm).withArgs(iBytes)
@@ -61,9 +129,15 @@ object Voting {
       .withNonce(0L)
 
     for {
-      _ <- DeployService[F].deploy(deploy)
-      _ <- DeployService[F].createBlock()
-    } yield ()
+      _      <- DeployService[F].deploy(deploy).rethrow
+      result <- DeployService[F].createBlock().rethrow
+      hash <- if (result.startsWith("Success"))
+               result.split("Block")(1).trim.split('.').head.pure[F]
+             else
+               Sync[F].raiseError(
+                 new RuntimeException(s"Unexpected response from block creation: $result")
+               )
+    } yield hash
   }
 }
 
@@ -80,9 +154,14 @@ object Radio {
   val default: Box[SatoshiIdentity] = Empty
   val radio: ChoiceHolder[SatoshiIdentity] =
     SHtml.radioElem(options, default) { selectedBox =>
-      val selected   = selectedBox.openOr(hal)
-      val votingTask = Voting.vote[Task](selected)(Sync[Task], Voting.deployService)
+      val selected = selectedBox.openOr(hal)
+      val votingTask = for {
+        hash <- Voting.vote[Task](selected)(Sync[Task], Voting.deployService)
+        _    <- Querying.latestBlock.set(Some(hash))
+      } yield ()
+
       votingTask.runSyncUnsafe()
+      S.redirectTo("/results")
     }
 
   def render = ".options" #> radio.toForm
