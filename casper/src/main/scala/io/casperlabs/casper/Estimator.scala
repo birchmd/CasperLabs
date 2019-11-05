1bde8ef8833296a830adae613c975964c91c4b18
birchmd <birchmd@casperlabs.io>
2019-11-05 14:34:37 -0500

    CON-489: New forkchoice rule

--- casper/src/main/scala/io/casperlabs/casper/Estimator.scala
+++ casper/src/main/scala/io/casperlabs/casper/Estimator.scala
@@ -23,130 +23,148 @@ object Estimator {
       genesis: BlockHash,
       latestMessageHashes: Map[Validator, Set[BlockHash]],
       equivocators: Set[Validator]
-  ): F[List[BlockHash]] = {
+  ): F[List[BlockHash]] =
+    latestHashesToLatestMessage[F](dag, latestMessageHashes, equivocators) flatMap {
+      honestValidators =>
+        val honestTips = honestValidators.map(_._2.messageHash)
 
-    /** Eliminate any latest message which has a descendant which is a latest message
-      * of another validator, because in that case those descendants should be the tips. */
-    def tipsOfLatestMessages(
-        latestMessages: List[BlockHash],
-        stopHash: BlockHash
-    ): F[List[Message]] =
-      if (latestMessages.isEmpty) dag.lookup(genesis).map(_.toList)
-      else {
-        // Start from the highest latest messages and traverse backwards
-        implicit val ord = DagOperations.blockTopoOrderingDesc
-        for {
-          latestMessagesMeta <- latestMessages.traverse(dag.lookup).map(_.flatten)
-          tips <- DagOperations
-                   .bfToposortTraverseF[F](latestMessagesMeta)(
-                     _.parents.toList.traverse(dag.lookup(_)).map(_.flatten)
-                   )
-                   .takeUntil(_.messageHash == stopHash)
-                   // We start with the tips and remove any message
-                   // that is reachable through the parent-child link from other tips.
-                   // This should leave us only with the tips that cannot be reached from others.
-                   .foldLeft(latestMessagesMeta.toSet) {
-                     case (tips, message) =>
-                       tips.filterNot(msg => message.parents.toSet.contains(msg.messageHash))
-                   }
-        } yield tips.toList
-      }
+        NonEmptyList.fromList(honestTips) match {
+          case None => List(genesis).pure[F]
 
-    val latestMessagesFlattened = latestMessageHashes.values.flatten.toList
+          case Some(tips) =>
+            for {
+              lca    <- DagOperations.latestCommonAncestorsMainParent(dag, tips)
+              result <- forkChoiceLoop(List(lca), honestValidators, dag)
+              // Since the fork-choice rule only looks at main parents, it is
+              // possible that some of the chosen secondary parents are reachable
+              // from the chosen main parent. Therefore, we look for and remove
+              // and such redundancies. Note that it may also be the case that
+              // some blocks in the secondary parents are redundant amongst
+              // themselves, however we do not remove them at this stage since
+              // they may or may not be used during merging.
+              redundancies <- DagOperations.collectWhereDescendantPathExists[F](
+                               dag,
+                               result.tail.toSet,
+                               Set(result.head)
+                             )
+            } yield result.filter(!redundancies(_))
+        }
+    }
 
-    for {
-      lca <- NonEmptyList
-              .fromList(latestMessagesFlattened)
-              .fold(genesis.pure[F])(DagOperations.latestCommonAncestorsMainParent(dag, _))
-      scores        <- lmdScoring(dag, lca, latestMessageHashes, equivocators)
-      newMainParent <- forkChoiceTip(dag, lca, scores)
-      parents       <- tipsOfLatestMessages(latestMessagesFlattened, lca)
-      secondaryParents = parents.filter(_.messageHash != newMainParent).filterNot { message =>
-        // Filter out blocks created by equivocators from the secondary parents.
-        // Secondary parents are not subject to the fork choice rule, the only requirement
-        // is that they don't conflict with the main chain. This opens up possibility for various
-        // kinds of attacks. An example could be a block created in the past, that includes a deploy
-        // that should have expired by now, if that block did not conflict with the main parent
-        // fork-choice would include it in the p-dag.
-        equivocators.contains(message.validatorId)
-      }
-      sortedSecParents = secondaryParents
-        .sortBy(b => scores.getOrElse(b.messageHash, Zero) -> b.messageHash.toStringUtf8)
-        .reverse
-    } yield newMainParent +: sortedSecParents.map(_.messageHash)
+  /**
+    * Looks up the latest message for each honest validator from
+    * the latest message hash. Note this is unique because honest
+    * validators have not equivocated, so have a unique latest message.
+    */
+  private def latestHashesToLatestMessage[F[_]: Monad](
+      dag: DagRepresentation[F],
+      latestMessageHashes: Map[Validator, Set[BlockHash]],
+      equivocators: Set[Validator]
+  ): F[List[(Validator, Message)]] =
+    latestMessageHashes
+      .filterKeys(!equivocators(_))
+      .toList
+      .traverse { case (v, lms) => lms.headOption.flatTraverse(dag.lookup).map(v -> _) }
+      .map(_.collect { case (v, Some(m)) => v -> m })
+
+  /**
+    * Finds the latest message of validator `v` voting for block `b`, if any.
+    */
+  private def latestMessageForBlock[F[_]: MonadThrowable](
+      b: BlockHash,
+      v: Validator,
+      latestMessage: Message,
+      dag: DagRepresentation[F]
+  ): F[Option[Message]] =
+    DagOperations
+      .swimlaneV[F](v, latestMessage, dag)
+      .findF(isMainAncestor(b, _, dag))
+
+  private def isMainAncestor[F[_]: MonadThrowable](
+      smallerRank: BlockHash,
+      largerRank: Message,
+      dag: DagRepresentation[F]
+  ): F[Boolean] = dag.lookup(smallerRank) flatMap {
+    case None          => false.pure[F]
+    case Some(message) => DagOperations.isMainAncestor(message, largerRank, dag)
   }
 
-  /** Computes scores for LMD GHOST.
-    *
-    * Starts at the latest messages from currently bonded validators
-    * and traverses up to the stop hash, collecting blocks' scores
-    * (which is the weight of validators who include that block as main parent of their block).
-    *
-    * @param stopHash Block at which we stop computing scores. Should be latest common ancestor of `latestMessagesHashes`.
-    * @return Scores map.
+  private def forkChoiceLoop[F[_]: MonadThrowable](
+      result: List[BlockHash],
+      honestLatestMessages: List[(Validator, Message)],
+      dag: DagRepresentation[F]
+  ): F[List[BlockHash]] =
+    result
+      .traverse { block =>
+        orderChildren[F](block, honestLatestMessages, dag)
+      }
+      .flatMap { orderedChildren =>
+        val newResult = orderedChildren.flatten
+
+        if (result == newResult) result.pure[F]
+        else forkChoiceLoop(newResult, honestLatestMessages, dag)
+      }
+
+  /**
+    * Returns the children of `block` in the main tree which are visible from the
+    * given latest messages.
     */
-  def lmdScoring[F[_]: MonadThrowable](
-      dag: DagRepresentation[F],
-      stopHash: BlockHash,
-      latestMessageHashes: Map[Validator, Set[BlockHash]],
-      equivocatingValidators: Set[Validator]
-  ): F[Map[BlockHash, Weight]] = {
-    implicit val decreasingOrder = Ordering[Long].reverse
-    latestMessageHashes.toList.foldLeftM(Map.empty[BlockHash, Weight]) {
-      case (acc, (validator, latestMessageHashes)) =>
-        for {
-          sortedMessages <- latestMessageHashes.toList
-                             .traverse(dag.lookup(_))
-                             .map(_.flatten.sortBy(_.rank))
-          lmdScore <- DagOperations
-                       .bfTraverseF[F, Message](sortedMessages)(
-                         _.parents.take(1).toList.traverse(dag.lookup(_)).map(_.flatten)
-                       )
-                       .takeUntil(_.messageHash == stopHash)
-                       .foldLeftF(acc) {
-                         case (acc2, message) =>
-                           (if (equivocatingValidators.contains(validator)) {
-                              Zero.pure[F]
-                            } else {
-                              weightFromValidatorByDag(dag, message.messageHash, validator)
-                            }).map { realWeight =>
-                             val oldValue = acc2.getOrElse(message.messageHash, Zero)
-                             acc2.updated(message.messageHash, realWeight + oldValue)
-                           }
-                       }
-        } yield lmdScore
+  private def getMainChildrenInView[F[_]: MonadThrowable](
+      block: BlockHash,
+      honestLatestMessages: List[(Validator, Message)],
+      dag: DagRepresentation[F]
+  ): F[List[BlockHash]] = dag.getMainChildren(block) flatMap { children =>
+    val lms = honestLatestMessages.map(_._2)
+
+    // keep children which can be reached from latest message justifications
+    children.filterA { child =>
+      DagOperations.toposortJDagDesc[F](dag, lms).find(m => m.messageHash == child).map(_.nonEmpty)
     }
   }
 
   /**
-    * Computes fork choice.
-    *
-    * @param dag Representation of the Block DAG.
-    * @param startingBlock Starting block for the fork choice rule.
-    * @param scores Map of block's scores.
-    * @return Block hash chosen by the fork choice rule.
+    * Orders the children of `block` by how much weight votes for each of them
+    * (using block hash has a tie breaker). If `block` has no children, then the
+    * block itself is returned.
     */
-  def forkChoiceTip[F[_]: Monad](
-      dag: DagRepresentation[F],
-      startingBlock: BlockHash,
-      scores: Map[BlockHash, Weight]
-  ): F[BlockHash] =
-    dag.getMainChildren(startingBlock).flatMap { mainChildren =>
-      {
-        // make sure they are reachable from latestMessages
-        val reachableMainChildren = mainChildren.filter(scores.contains)
-        if (reachableMainChildren.isEmpty) {
-          startingBlock.pure[F]
-        } else {
-          val highestScoreChild =
-            reachableMainChildren.maxBy(b => scores(b) -> b.toStringUtf8)
-          forkChoiceTip[F](
-            dag,
-            highestScoreChild,
-            scores
-          )
-        }
-      }
-    }
+  private def orderChildren[F[_]: MonadThrowable](
+      block: BlockHash,
+      honestLatestMessages: List[(Validator, Message)],
+      dag: DagRepresentation[F]
+  ): F[List[BlockHash]] = getMainChildrenInView[F](block, honestLatestMessages, dag) flatMap {
+    case Nil => List(block).pure[F]
 
+    case children =>
+      honestLatestMessages
+        .traverse { case (v, lm) => latestMessageForBlock[F](block, v, lm, dag).map(v -> _) }
+        .map(_.collect { case (v, Some(lm)) if lm.messageHash != block => v -> lm })
+        .flatMap(_.traverse {
+          case (v, latestMessage) =>
+            for {
+              weight     <- weightFromValidatorByDag[F](dag, block, v)
+              maybeChild <- children.findM(child => isMainAncestor[F](child, latestMessage, dag))
+              child <- maybeChild.fold(
+                        // This case should never happen because if `latestMessageForBlock`
+                        // returned `Some(latestMessage)` then `block` is an ancestor of
+                        // `latestMessage`, which means either one of the children of `block`
+                        // must also be an ancestor of `latestMessage`, or `latestMessage`
+                        // equals `block`. The latter case is taken care of in the
+                        // `collect` statement where we explicitly filter out latest messages
+                        // equal to the current block. Note that this filtering is valid
+                        // because in that case the validator does not vote for any child.
+                        MonadThrowable[F].raiseError[BlockHash](
+                          new Exception("Latest message votes for no child!")
+                        )
+                      )(child => child.pure[F])
+            } yield (child, weight)
+        }.map { votes =>
+          val scores = votes
+            .groupBy(_._1)
+            .mapValues(_.foldLeft(Zero) { case (acc, (_, weight)) => acc + weight })
+
+          children.sortBy(child => scores.getOrElse(child, Zero) -> child.toStringUtf8)(
+            Ordering[(BigInt, String)].reverse
+          )
+        })
+  }
 }
